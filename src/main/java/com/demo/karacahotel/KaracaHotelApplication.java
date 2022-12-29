5347f50b9ae98236ad23b7c32a65d91093067836
selimkucukkaraca <selimkkkaraca@hotmail.com>
2022-12-29 05:10:21 +0300

    Initial commit

---
+++ src/main/java/com/demo/karacahotel/KaracaHotelApplication.java
@@ -0,0 +1,13 @@
+package com.demo.karacahotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaracaHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaracaHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/karacahotel/KaracaHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.karacahotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaracaHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
